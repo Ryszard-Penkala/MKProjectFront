992b55f3563d929ff1cf35c7912bb45ee95e5073
Ryszard-Penkala <ryszard.penkala@gmail.com>
2022-05-20 01:05:34 +0200

    Integrated Map & Popup icon.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,8 +1,26 @@
 import React from "react";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
+import '../../utils/fix-map-icon'
+
+import 'leaflet/dist/leaflet.css'
 import './Map.css'
 
 export const Map = () => {
     return (
-        <div className="map"/>
+        <div className="map">
+            <MapContainer center={[50.2657152, 18.9945008]} zoom={13}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href='https://www.opentreetmap.org/copyright'>OpenStreetMap</a> contributors"
+                />
+                <Marker position={[50.2657152, 18.9945008]}>
+                    <Popup>
+                        <h2>IT.focus</h2>
+                        <p>Firma programistyczna</p>
+                    </Popup>
+
+                </Marker>
+            </MapContainer>
+        </div>
     )
 }
